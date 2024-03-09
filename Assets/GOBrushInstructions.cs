65120724e6c795e4b5b22400d3cca7f23e020faa
APBCh33se22 <aidanbrophy@comcast.net>
2024-03-09 12:27:56 -0500

    GameObject Brush Working
    Figured out the Game Object brush, laid the groundwork to fill the map, physics works for existing "prefab" Game Object trees.

---
+++ Assets/GOBrushInstructions.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README : MonoBehaviour
+{
+    //To make the GameObject brush work
+    //1. Open the Trees tilemap
+    //2. Open the Tile Palette
+    //3. Ensure that "Paint on sceneroot" is selected
+    //4. Select the eyedropper
+    //5. Use the eyedropper in the scene to select
+    //the desired Game Object(s)
+    //6. Use brush on the scene
+}
