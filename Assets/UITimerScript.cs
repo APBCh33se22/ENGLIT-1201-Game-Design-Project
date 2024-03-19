1ff558c7206cb6eaad5a1f3606bcb3d1b32e9c32
APBCh33se22 <aidanbrophy@comcast.net>
2024-03-19 10:16:34 -0400

    Update UITimerScript.cs

--- Assets/UITimerScript.cs
+++ Assets/UITimerScript.cs
@@ -9,13 +9,20 @@ public class UITimerScript : MonoBehaviour
    public float timeRemaining = 600;
    public bool timerIsRunning = false;
    public TextMeshProUGUI timerText;
+   public Sprite player;
+   public GameObject finish_line;
+   public bool finished = false;
 
    private void Start() {
         timerIsRunning = true;
    }
 
    void Update() {
-        if (timerIsRunning) {
+        if (finished) {
+
+        }
+        
+        else if (timerIsRunning) {
             if (timeRemaining > 0) {
                 timeRemaining -= Time.deltaTime;
             }
@@ -33,5 +40,9 @@ public class UITimerScript : MonoBehaviour
     float minutes = Mathf.FloorToInt(timeToDisplay/60);
     float seconds = Mathf.FloorToInt(timeToDisplay%60);
     timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+   }
+
+   void check_finish_line() {
+    
    }
 }
