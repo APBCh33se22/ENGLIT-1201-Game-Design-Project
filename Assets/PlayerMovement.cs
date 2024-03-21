8db061366130be57048705f37571978895750ae4
APBCh33se22 <aidanbrophy@comcast.net>
2024-03-21 08:58:46 -0400

    Physics is actually working this time
    No modifications to the move script necessary

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
     Vector2 movement;
+    public ContactFilter2D moveFilter;
+    private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     // Update is called once per frame
     void Update()
@@ -18,7 +21,22 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + (movement * moveSpeed * Time.fixedDeltaTime));
+        //bool success = playerMovement(movement);
+
     }
+    /*
+    private bool playerMovement(Vector2 direction)
+    {
+         int count = rb.Cast(direction, moveFilter,castCollisions, direction * Time.fixedDeltaTime);
+        if (count == 0) { //No collisions, move as usual
+            // Movement
+            rb.MovePosition(rb.position + (direction * moveSpeed * Time.fixedDeltaTime));
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    */
 }
